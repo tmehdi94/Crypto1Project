fbdba4af22d37837dc184ea7e317089795f42eb5
Pratik Patel <prpatel05@gmail.com>
2014-11-17 10:51:18 -0800

    Fixed errors, added mutex, updated account class

--- bank.cpp
+++ bank.cpp
@@ -1,7 +1,7 @@
 /**
-	@file bank.cpp
-	@brief Top level bank implementation file
- */
+  @file bank.cpp
+  @brief Top level bank implementation file
+  */
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,275 +14,393 @@
 #include <iostream>
 #include <vector>
 
-
 void* client_thread(void* arg);
 void* console_thread(void* arg);
 
-class Account{
-	// mutex so multiple cant access account at same time??
-  public:
-  	std::string name;
-	int balance;
-	int pin;
-  	Account(std::string, int, int);
-  	Account();
-  	Account & operator= (const Account & a){
-  		if (this != &a){
-            this->name = a.name;
-            this->balance = a.balance;
-            this->pin = a.pin;
-        }
-        return *this;
-  	}
+class Account
+{
+    public:
+        Account(std::string, int, int);
+        Account();
+        Account & operator= (const Account & a);
+        std::string getName();
+        int getBalance();
+        void setBalance(int b);
+        int getPin();
+        void setPin(int p);
+        bool withdraw(int amount);
+        bool deposit(int amount);
+        bool transfer(int amount, Account other);
+
+    private:
+        std::string name;
+        int balance;
+        int pin;
+        pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 };
 
-Account::Account (std::string n, int m, int p){
-	balance = m;
-	name = n;
-	pin = p;
-}
-Account::Account (){
-	balance = 0;
-	name = "";
-	pin = 0000;
+Account::Account (std::string n, int m, int p)
+{
+    balance = m;
+    name = n;
+    pin = p;
 }
 
+Account::Account ()
+{
+    balance = 0;
+    name = "";
+    pin = 0000;
+}
 
+Account &Account::operator= (const Account & a)
+{
+    if (this != &a){
+        this->name = a.name;
+        this->balance = a.balance;
+        this->pin = a.pin;
+    }
+    return *this;
+}
 
+std::string Account::getName()
+{
+    return name;
+}
+
+int Account::getBalance()
+{
+    return balance;
+}
+
+void Account::setBalance(int b)
+{
+    balance = b;
+}
+
+int Account::getPin()
+{
+    return pin;
+}
+
+void Account::setPin(int p)
+{
+    pin = p;
+}
+
+bool Account::withdraw(int amount)
+{
+    pthread_mutex_lock(&lock);
+    bool status = false;
+    if(balance > amount)
+    {
+        balance -= amount;
+        status = true;
+    }
+    else
+    {
+        status = false;
+    }
+    pthread_mutex_unlock(&lock);
+    return status;
+}
+
+bool Account::deposit(int amount)
+{
+    //TODO overflow check
+    pthread_mutex_lock(&lock);
+    balance += amount;
+    pthread_mutex_unlock(&lock);
+    return true;
+}
+
+bool Account::transfer(int amount, Account other)
+{
+    pthread_mutex_lock(&lock);
+    bool status = false;
+    if(balance > amount)
+    {
+        balance -= amount;
+        status = true;
+    }
+    else
+    {
+        status = false;
+    }
+
+    other.deposit(amount);
+    pthread_mutex_unlock(&lock);
+}
+
+//Perhaps we should write this to a file so a power out attack cant erase accounts
 std::vector<Account> Accounts;
-
 
 int main(int argc, char* argv[])
 {
-	Account a("Alice", 100, 1234);
-	Account b("Bob", 50, 1234);
-	Account e("Eve", 0 , 1234);
-	Accounts.push_back(a);
-	Accounts.push_back(b);
-	Accounts.push_back(e);
+    Account a("Alice", 100, 1234);
+    Account b("Bob", 50, 1234);
+    Account e("Eve", 0 , 1234);
+    Accounts.push_back(a);
+    Accounts.push_back(b);
+    Accounts.push_back(e);
 
-	if(argc != 2)
-	{
-		printf("Usage: bank listen-port\n");
-		return -1;
-	}
-	
-	unsigned short ourport = atoi(argv[1]);
-	
-	//socket setup
-	int lsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(!lsock)
-	{
-		printf("fail to create socket\n");
-		return -1;
-	}
-	
-	//listening address
-	sockaddr_in addr_l;
-	addr_l.sin_family = AF_INET;
-	addr_l.sin_port = htons(ourport);
-	unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr_l.sin_addr);
-	ipaddr[0] = 127;
-	ipaddr[1] = 0;
-	ipaddr[2] = 0;
-	ipaddr[3] = 1;
-	if(0 != bind(lsock, reinterpret_cast<sockaddr*>(&addr_l), sizeof(addr_l)))
-	{
-		printf("failed to bind socket\n");
-		return -1;
-	}
-	if(0 != listen(lsock, SOMAXCONN))
-	{
-		printf("failed to listen on socket\n");
-		return -1;
-	}
-	
-	pthread_t cthread;
-	pthread_create(&cthread, NULL, console_thread, NULL);
-	
-	//loop forever accepting new connections
-	while(1)
-	{
-		sockaddr_in unused;
-		socklen_t size = sizeof(unused);
-		int csock = accept(lsock, reinterpret_cast<sockaddr*>(&unused), &size);
-		if(csock < 0)	//bad client, skip it
-			continue;
-			
-		pthread_t thread;
-		pthread_create(&thread, NULL, client_thread, (void*)csock);
-	}
+    if(argc != 2)
+    {
+        printf("Usage: bank listen-port\n");
+        return -1;
+    }
+
+    unsigned short ourport = atoi(argv[1]);
+
+    //socket setup
+    int lsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(!lsock)
+    {
+        printf("fail to create socket\n");
+        return -1;
+    }
+
+    //listening address
+    sockaddr_in addr_l;
+    addr_l.sin_family = AF_INET;
+    addr_l.sin_port = htons(ourport);
+    unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr_l.sin_addr);
+    ipaddr[0] = 127;
+    ipaddr[1] = 0;
+    ipaddr[2] = 0;
+    ipaddr[3] = 1;
+    if(0 != bind(lsock, reinterpret_cast<sockaddr*>(&addr_l), sizeof(addr_l)))
+    {
+        printf("failed to bind socket\n");
+        return -1;
+    }
+    if(0 != listen(lsock, SOMAXCONN))
+    {
+        printf("failed to listen on socket\n");
+        return -1;
+    }
+
+    pthread_t cthread;
+    pthread_create(&cthread, NULL, console_thread, NULL);
+
+    //loop forever accepting new connections
+    while(1)
+    {
+        sockaddr_in unused;
+        socklen_t size = sizeof(unused);
+        int csock = accept(lsock, reinterpret_cast<sockaddr*>(&unused), &size);
+        if(csock < 0)	//bad client, skip it
+            continue;
+
+        pthread_t thread;
+        pthread_create(&thread, NULL, client_thread, (void*)&csock);
+    }
 }
 
 void* client_thread(void* arg)
 {
-	int csock = (int)arg;
-	
-	printf("[bank] client ID #%d connected\n", csock);
-	
-	//input loop
-	int length;
-	char packet[1024];
-	Account* current;
-	while(1)
-	{
-		//read the packet from the ATM
-		if(sizeof(int) != recv(csock, &length, sizeof(int), 0))
-			break;
-		if(length >= 1024)
-		{
-			printf("packet too long\n");
-			break;
-		}
-		if(length != recv(csock, packet, length, 0))
-		{
-			printf("[bank] fail to read packet\n");
-			break;
-		}
-		
-		//TODO: process packet data
+    int csock = *(int*)arg;
 
-		//decrypt and authenticate
-		//store in buffer after decryption
+    printf("[bank] client ID #%d connected\n", csock);
 
-		std::vector<std::string> commands;
-		char hold[strlen(packet)];
-		strcpy(hold, packet);
-		//char* hold = buffer;
-		char* token = strtok(hold," ");
-		int i = 0;
-		while(token != NULL){
-			commands[i] = std::string(token);
-			i++;
-			token = strtok(NULL," ");
-		}
-		std::string buffer;
+    //input loop
+    int length;
+    char packet[1024];
+    Account* current;
+    while(1)
+    {
+        //read the packet from the ATM
+        if(sizeof(int) != recv(csock, &length, sizeof(int), 0))
+            break;
+        if(length >= 1024)
+        {
+            printf("packet too long\n");
+            break;
+        }
+        if(length != recv(csock, packet, length, 0))
+        {
+            printf("[bank] fail to read packet\n");
+            break;
+        }
 
-		if(commands[0] == "login"){
-			for(int i = 0; i < Accounts.size(); i++){
-				if(commands[1] == Accounts[i].name){
-					char * pin;
-					//read for pin
-					if(atoi(pin) == Accounts[i].pin){
-						buffer = "Logged in";
-						current = &Accounts[i];
-						break;
-					}
-				}
-			}
-			if(current == NULL){
-				//login and pin dont match
-				buffer = "Username and PIN don't match";
-			}
+        //TODO: process packet data
 
-		}
-		else if(commands[0] == "balance"){
-			buffer = current->balance;
-		}
-		else if(commands[0] == "withdraw"){
-			//lock mutex
-			if(current->balance > atoi(commands[1].c_str())){
-				current->balance -= atoi(commands[1].c_str());
-				buffer = commands[1] + " withdrawn";
-			}
-			else{
-				buffer = "Insufficient funds";
-			}
-			//unlock mutex
-		}
-		else if(commands[0] == "transfer"){
-			//lock mutex
-			if(current->balance > atoi(commands[1].c_str())){
-				int i;
-				for(i = 0; i < Accounts.size(); i++){
-					if(Accounts[i].name == commands[2]){
-						current->balance -= atoi(commands[1].c_str());
-						Accounts[i].balance += atoi(commands[1].c_str());
-						buffer = commands[1] + " Transferred to " + commands[2];
-						break;
-					}
-				}
-				if(i == Accounts.size()){
-					//user account not found;
-					buffer = "User could not be found";
-				}
-			}
-			else{
-				buffer = "Insufficient funds";
-			}
-			//unlock mutex
-		}
-		
-		length = buffer.size();
-		strcat(packet, buffer.c_str());
-		//encrypt
-		//TODO: put new data in packet
-		
-		//send the new packet back to the client
-		if(sizeof(int) != send(csock, &length, sizeof(int), 0))
-		{
-			printf("[bank] fail to send packet length\n");
-			break;
-		}
-		if(length != send(csock, (void*)packet, length, 0))
-		{
-			printf("[bank] fail to send packet\n");
-			break;
-		}
+        //decrypt and authenticate
+        //store in buffer after decryption
+        //unpad?
 
-	}
+        std::vector<std::string> commands;
+        char hold[strlen(packet)];
+        strcpy(hold, packet);
+        //char* hold = buffer;
+        char* token = strtok(hold," ");
+        int i = 0;
+        while(token != NULL)
+        {
+            commands[i] = std::string(token);
+            i++;
+            token = strtok(NULL," ");
+        }
+        std::string buffer;
 
-	printf("[bank] client ID #%d disconnected\n", csock);
+        if(commands[0] == "login")
+        {
+            for(int i = 0; i < Accounts.size(); i++)
+            {
+                if(commands[1] == Accounts[i].getName())
+                {
+                    char * pin;
+                    //read for pin
+                    if(atoi(pin) == Accounts[i].getPin())
+                    {
+                        buffer = "Logged in";
+                        current = &Accounts[i];
+                        break;
+                    }
+                }
+            }
+            if(current == NULL)
+            {
+                //login and pin dont match
+                buffer = "Username and PIN don't match";
+            }
 
-	close(csock);
-	return NULL;
+        }
+        else if(commands[0] == "balance")
+        {
+            buffer = current->getBalance();
+        }
+        else if(commands[0] == "withdraw")
+        {
+            if (current->withdraw(atoi(commands[1].c_str())))
+            {
+                buffer = commands[1] + " withdrawn";
+            }
+            else{
+                buffer = "Insufficient funds";
+            }
+        }
+        else if(commands[0] == "transfer")
+        {
+            Account *other = NULL;
+            for(unsigned int i = 0; i < Accounts.size(); i++)
+            {
+                if(Accounts[i].getName() == commands[2])
+                {
+                    other = &Accounts[i];
+                    break;
+                }
+            }
+
+            if(other == NULL)
+            {
+                //user account not found;
+                buffer = "User could not be found";
+            }
+            else
+            {
+                if (current->transfer(atoi(commands[1].c_str()), *other))
+                {
+                    buffer = commands[1] + " Transferred to " + commands[2];
+                }
+                else
+                {
+                    buffer = "Insufficient funds";
+                }
+            }
+        }
+
+        length = buffer.size();
+        strcat(packet, buffer.c_str());
+        //encrypt
+        //TODO: put new data in packet
+
+        //send the new packet back to the client
+        if(sizeof(int) != send(csock, &length, sizeof(int), 0))
+        {
+            printf("[bank] fail to send packet length\n");
+            break;
+        }
+        if(length != send(csock, (void*)packet, length, 0))
+        {
+            printf("[bank] fail to send packet\n");
+            break;
+        }
+
+    }
+
+    printf("[bank] client ID #%d disconnected\n", csock);
+
+    close(csock);
+    return NULL;
 }
 
 void* console_thread(void* arg)
 {
-	char buf[80];
-	while(1)
-	{
-		printf("bank> ");
-		fgets(buf, 79, stdin);
-		buf[strlen(buf)-1] = '\0';	//trim off trailing newline
-		
-		//TODO: your input parsing code has to go here
-		std::vector<std::string> commands;
-		char hold[strlen(buf)];
-		strcpy(hold, buf);
-		char * token = strtok(hold," ");
-		int i = 0;
-		while(token != NULL){
-			commands[i] = token;
-			i++;
-			token = strtok(NULL," ");
-		}
+    char buf[80];
+    while(1)
+    {
+        printf("bank> ");
+        fgets(buf, 79, stdin);
+        buf[strlen(buf)-1] = '\0';	//trim off trailing newline
+
+        //TODO: your input parsing code has to go here
+        std::vector<std::string> commands;
+        char hold[strlen(buf)];
+        strcpy(hold, buf);
+        char * token = strtok(hold," ");
+        int i = 0;
+        while(token != NULL)
+        {
+            commands[i] = token;
+            i++;
+            token = strtok(NULL," ");
+        }
 
 
-		if(commands[0] == "deposit"){
-			int i;
-			for(i = 0; i < Accounts.size(); i++){
-				if(Accounts[i].name == commands[1]){
-					//lock mutex
-					Accounts[i].balance += atoi(commands[2].c_str());
-					break;
-				}
-			}
-			if(i == Accounts.size()){
-				//account not found
-			}
-		}
-		else if(commands[0] == "balance"){
-			int i;
-			for(i = 0; i < Accounts.size(); i++){
-				if(Accounts[i].name == commands[1]){
-					//print account balance
-				}
-			}
-		}
-		else{
-			//not valid;
-		}
-	}
+        if(commands[0] == "deposit")
+        {
+            Account *current = NULL;
+            for(i = 0; i < Accounts.size(); i++)
+            {
+                if(Accounts[i].getName() == commands[1])
+                {
+                    current = &Accounts[i];
+                    break;
+                }
+            }
+            if(current == NULL)
+            {
+                printf("No account found\n");
+            }
+            else
+            {
+                current->deposit(atoi(commands[2].c_str()));
+            }
+        }
+        else if(commands[0] == "balance")
+        {
+            Account *current = NULL;
+            for(i = 0; i < Accounts.size(); i++)
+            {
+                if(Accounts[i].getName() == commands[1])
+                {
+                    current = &Accounts[i];
+                    break;
+                }
+            }
+            if (current == NULL)
+            {
+                printf("No account found\n");
+            }
+            else
+            {
+                printf("Balance: %d\n", current->getBalance());
+            }
+        }
+        else
+        {
+            //not valid;
+        }
+    }
 }
